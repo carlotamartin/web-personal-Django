602019570c5a9e8d0ff4368c74bb9bda0344555f
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-09 12:44:05 +0100

    cambios

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, HttpResponse
 #HttResponse nos permite constar a una petición devolviendo un código
 # Create your views here.
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about/">Acerca de</a></li>
+    </ul>
+"""
 
 def home(request):
-    return HttpResponse("<h1>Mi Web Personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+ <h2>Bienvenidos</h2>
+ <p>Esto es la portada.</p>
+ """)
+
+def about(request):
+    return HttpResponse("""
+        <h1>Mi Web Personal</h1>
+        <h2>Acerca de</h2>
+        <p>Me llamo Héctor y me encanta Django!</p>
+    """)
 
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -20,4 +20,5 @@ from core import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
