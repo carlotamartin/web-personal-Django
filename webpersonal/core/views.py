dedf7893b2f103db66c558ec19296eee8a0ac259
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-09 12:35:15 +0100

    probando nuevas formas para  el lenguaje HTML

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+#HttResponse nos permite constar a una petición devolviendo un código
 # Create your views here.
+
+def home(request):
+    html_response = "<h1>Mi Web Personal</h1>"
+    for i in range(10):
+        html_response += "<p>Esto es la portada</p>"
+    return HttpResponse(html_response)
+
