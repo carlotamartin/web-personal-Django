58f56f112d167c2a283fc935db86d1605cc88443
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-09 17:36:35 +0100

    empezamos a crear páginas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -6,19 +6,34 @@ html_base = """
     <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about/">Acerca de</a></li>
+    <li><a href="/contact/">Contacto</a></li>
     </ul>
 """
 
 def home(request):
     return HttpResponse(html_base + """
- <h2>Bienvenidos</h2>
- <p>Esto es la portada.</p>
- """)
+            <h2>Bienvenidos</h2>
+            <p>Esto es la portada.</p>
+    """)
 
 def about(request):
     return HttpResponse("""
         <h1>Mi Web Personal</h1>
         <h2>Acerca de</h2>
-        <p>Me llamo Héctor y me encanta Django!</p>
+        <p>Me llamo Carlota y me encantas TÚ!!!!</p>
     """)
+
+
+def contact(request):
+    return HttpResponse(html_base + """
+        <h2>Contacto</h2>
+        <p>Aquí os dejo mi email y mis redes sociales:</p>
+        <ul>
+        <li><a href="mailto:cmartmat@myuax.com">Email</a></li>
+        <li><a href="https://github.com/carlotamartin">Github</a></li>
+        <li><a href="https://youtube.com">Youtube</a></li>
+        </ul>
+
+        """)
+
 
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -19,6 +19,8 @@ from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
+
 ]
