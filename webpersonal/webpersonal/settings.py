9a345116b9507145ae9a676f1b2cd0948ef5f095
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-09 11:56:55 +0100

    cambio en la base de datos

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':
+        'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
