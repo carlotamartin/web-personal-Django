95b1d48507390f57b1a14997dd189e058ac1f6a5
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-09 18:37:33 +0100

    dan erroes

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,36 +1,17 @@
 from django.shortcuts import render, HttpResponse
 #HttResponse nos permite constar a una petición devolviendo un código
 # Create your views here.
-html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about/">Acerca de</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-    </ul>
-"""
+
 
 def home(request):
     return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse("""
-        <h1>Mi Web Personal</h1>
-        <h2>Acerca de</h2>
-        <p>Me llamo Carlota y me encantas TÚ!!!!</p>
-    """)
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
 
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contacto</h2>
-        <p>Aquí os dejo mi email y mis redes sociales:</p>
-        <ul>
-        <li><a href="mailto:cmartmat@myuax.com">Email</a></li>
-        <li><a href="https://github.com/carlotamartin">Github</a></li>
-        <li><a href="https://youtube.com">Youtube</a></li>
-        </ul>
-
-        """)
-
-
+    return render(request, "core/contact.html")
