55a4c6c3a94093290a986c89251819eeb601ee07
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-11 15:14:07 +0100

    cambios

--- webpersonal/portfolio/admin.py
+++ webpersonal/portfolio/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project
 # Register your models here.
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+admin.site.register(Project, ProjectAdmin)
