66de5c7ad737f55839b056c0105f97c2a53513be
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2022-11-20 13:33:36 +0100

    cambios

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'webpersonal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR , 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,13 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    BASE_DIR / "core/"
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
